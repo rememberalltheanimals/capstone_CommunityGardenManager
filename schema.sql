662a153e6f15e0de87744269dd4faa0129c73cbd
davisjklevgard <48463222+davisjklevgard@users.noreply.github.com>
2023-05-08 16:27:32 -0500

    Working on adding member info to a database

--- schema.sql
+++ schema.sql
@@ -8,6 +8,20 @@ CREATE TABLE watered (
   memberName TEXT NOT NULL
 );
 
+CREATE TABLE members (
+  id SERIAL PRIMARY KEY,
+  memberName TEXT NOT NULL,
+  userName TEXT NOT NULL,
+  email TEXT NOT NULL,
+  phoneNumber TEXT NOT NULL,
+  zipCode INT NOT NULL,
+  gardenTime TEXT NOT NULL,
+  memberPassword TEXT NOT NULL,
+  garden TEXT NOT NULL
+);
+
 INSERT INTO gardenButtons (name) VALUES ('Eunhae');
 INSERT INTO gardenButtons (name) VALUES ('Levi');
 INSERT INTO gardenButtons (name) VALUES ('Davis');
+INSERT INTO members (memberName, userName, email, phoneNumber, zipCode, gardenTime, memberPassword, garden)
+VALUES ();
